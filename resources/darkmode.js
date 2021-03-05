087bfedefd38bc1aaf7bbf913a7cae96a4a70e0b
Joshua Ghali <jjghali@protonmail.ch>
2021-03-04 23:06:53 -0500

    icon switch

--- resources/darkmode.js
+++ resources/darkmode.js
@@ -1,18 +1,28 @@
 $(document).ready(function () {
-    $(".darkmode-toggle").change(function () {
+    $(".darkmode-toggle").change(function (event) {
+        // event.preventDefault();
         if (this.checked) {
+
             less.modifyVars({
                 '@primary-color': '@primary-color-light',
                 '@secondary-color': '@secondary-color-light',
                 '@background-color': '@background-color-light'
-            });
+            }).then(() => {
+                $(".darkmodeicon > div > i").removeClass("fa-moon")
+                $(".darkmodeicon > div > i").addClass("fa-sun")
+            })
         }
         else {
             less.modifyVars({
                 '@primary-color': '@primary-color-dark',
                 '@secondary-color': '@secondary-color-dark',
                 '@background-color': '@background-color-dark'
-            });
+            }).then(() => {
+                $(".darkmodeicon > div > i").removeClass("fa-sun")
+                $(".darkmodeicon > div > i").addClass("fa-moon")
+            })
         }
+
+
     })
 })

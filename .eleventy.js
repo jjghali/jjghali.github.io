0624d8f678a139305f447698add3c06d2ea2cee4
Joshua Ghali <jjghali@protonmail.ch>
2021-08-31 22:55:44 -0400

    css

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,4 @@
+const htmlmin = require('html-minifier')
 const now = String(Date.now())
 
 module.exports = function (eleventyConfig) {
@@ -8,7 +9,28 @@ module.exports = function (eleventyConfig) {
         './_tmp/style.css': './style.css'
     });
 
+    eleventyConfig.addPassthroughCopy('public');
+
     eleventyConfig.addShortcode('version', function () {
         return now
     })
+
+
+
+eleventyConfig.addTransform('htmlmin', function (content, outputPath) {
+    if (
+      process.env.ELEVENTY_PRODUCTION &&
+      outputPath &&
+      outputPath.endsWith('.html')
+    ) {
+      let minified = htmlmin.minify(content, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true,
+      });
+      return minified
+    }
+
+    return content
+})
 };
--- styles/tailwind.config.js
+++ styles/tailwind.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  darkMode: 'media',
     purge: {
       content: ['_site/**/*.html'],
       options: {

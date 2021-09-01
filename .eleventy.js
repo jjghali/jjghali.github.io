317c7f6450685b9113d986b91c842a544ffd9c55
Joshua Ghali <jjghali@protonmail.ch>
2021-08-31 21:25:25 -0400

    init 11ty projct

---
+++ .eleventy.js
@@ -0,0 +1,14 @@
+const now = String(Date.now())
+
+module.exports = function (eleventyConfig) {
+    eleventyConfig.setUseGitIgnore(false);
+    eleventyConfig.addWatchTarget('./_tmp/style.css');
+
+    eleventyConfig.addPassthroughCopy({
+        './_tmp/style.css': './style.css'
+    });
+
+    eleventyConfig.addShortcode('version', function () {
+        return now
+    })
+};
